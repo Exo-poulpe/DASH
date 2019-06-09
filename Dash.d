445224655f3edd1988db415426b5233976c4b9f3
unknown <exo.poulpe@outlook.com>
2019-06-09 11:04:24 +0200

    Add sha256 & 512 + add info print

--- Dash.d
+++ Dash.d
@@ -6,21 +6,28 @@ import std.stdio;
 import std.string;
 import std.datetime;
 
+immutable string POSITIVE = "[+]";
+immutable string NEGATIVE = "[-]";
+immutable string SPECIAL = "[*]";
+immutable string VERSION = "0.1.0.1";
+
 string Target = null, Wordlist = null;
-bool Verbose = false;
+bool Verbose = false, Counter;
 int Mode = -1;
 
-string HelpMode = "Mode to use for hash function\n\t0 | md5\n\t1 | sha1";
+string HelpMode = "Mode to use for hash function\n\t0 | md5\n\t1 | sha1\n\t2 | sha256\n\t3 | sha512";
 
 int main(string[] args)
 {
-    auto parser = getopt(args, "target|t", "Target value to find", &Target, "mode|m", HelpMode, &Mode, "wordlist|w",
-            "Wordlist to use for password testing", &Wordlist, "verbose|v",
-            "More verbose output", &Verbose);
+    auto parser = getopt(args, "target|t", "Target value to find", &Target,
+            "mode|m", HelpMode, &Mode, "count", "Print count password only",
+            &Counter, "wordlist|w", "Wordlist to use for password testing",
+            &Wordlist, "verbose|v", "More verbose output", &Verbose);
 
     if (parser.helpWanted)
     {
         defaultGetoptPrinter("This program break hash from D language.", parser.options);
+        writefln("\nProgram write by Exo-poulpe %s",VERSION);
     }
     else if (Target != null && Mode != -1 && Wordlist != null)
     {
@@ -33,11 +40,11 @@ int main(string[] args)
                 string tmp = HashTesting(Target, Wordlist, new MD5Digest());
                 if (tmp != "")
                 {
-                    writefln("Password found \n%s:%s", Target, tmp);
+                    writefln("%sPassword found \n%s:%s", POSITIVE, Target, tmp);
                 }
                 else
                 {
-                    writeln("Password not found");
+                    writefln("%sPassword not found", NEGATIVE);
                 }
                 break;
 
@@ -46,11 +53,35 @@ int main(string[] args)
                 string tmp = HashTesting(Target, Wordlist, new SHA1Digest());
                 if (tmp != "")
                 {
-                    writefln("Password found \n%s:%s", Target, tmp);
+                    writefln("%sPassword found \n%s:%s", POSITIVE, Target, tmp);
                 }
                 else
                 {
-                    writeln("Password not found");
+                    writefln("%sPassword not found", NEGATIVE);
+                }
+                break;
+            case 2:
+                writeln("Mode : SHA256");
+                string tmp = HashTesting(Target, Wordlist, new SHA256Digest());
+                if (tmp != "")
+                {
+                    writefln("%sPassword found \n%s:%s", POSITIVE, Target, tmp);
+                }
+                else
+                {
+                    writefln("%sPassword not found", NEGATIVE);
+                }
+                break;
+            case 3:
+                writeln("Mode : SHA512");
+                string tmp = HashTesting(Target, Wordlist, new SHA512Digest());
+                if (tmp != "")
+                {
+                    writefln("%sPassword found \n%s:%s", POSITIVE, Target, tmp);
+                }
+                else
+                {
+                    writefln("%sPassword not found", NEGATIVE);
                 }
                 break;
 
@@ -68,6 +99,7 @@ int main(string[] args)
     else
     {
         defaultGetoptPrinter("This program break hash from D language.", parser.options);
+        writefln("\nProgram write by Exo-poulpe %s",VERSION);
     }
 
     return 0;
@@ -91,7 +123,7 @@ string HashTesting(string hash, string wordlist, Digest mode)
         }
         if (hashResult == hash)
         {
-            if (Verbose)
+            if (Verbose || Counter)
             {
                 writefln("Password count : %u", count);
             }
